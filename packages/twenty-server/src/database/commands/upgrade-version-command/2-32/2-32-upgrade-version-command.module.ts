547f6fb5fbd76f9809166a2b4f465196d027f737
dazspec <268620957+dazspec@users.noreply.github.com>
2026-08-12 12:07:40 -0400

    Make required fields without defaults optional on program objects

---
+++ packages/twenty-server/src/database/commands/upgrade-version-command/2-32/2-32-upgrade-version-command.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+
+import { WorkspaceIteratorModule } from 'src/database/commands/command-runners/workspace-iterator.module';
+import { MakeRequiredFieldsWithoutDefaultsOptionalCommand } from 'src/database/commands/upgrade-version-command/2-32/2-32-workspace-command-1786900000000-make-required-fields-without-defaults-optional.command';
+import { ApplicationModule } from 'src/engine/core-modules/application/application.module';
+import { WorkspaceCacheModule } from 'src/engine/workspace-cache/workspace-cache.module';
+import { WorkspaceMigrationModule } from 'src/engine/workspace-manager/workspace-migration/workspace-migration.module';
+
+@Module({
+  imports: [
+    ApplicationModule,
+    WorkspaceCacheModule,
+    WorkspaceMigrationModule,
+    WorkspaceIteratorModule,
+  ],
+  providers: [MakeRequiredFieldsWithoutDefaultsOptionalCommand],
+})
+export class V2_32_UpgradeVersionCommandModule {}
